14966e4185d6dec6a8cf6ad0dd57ae46d7174cf9
Vedant Tiwari <tiwarivedant.1690@gmail.com>
2021-08-31 13:53:17 +0530

    learn about styles routes,classes in flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_f1/pages/home_page.dart';
+import 'package:flutter_f1/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,6 +10,18 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: HomePage());
+    return MaterialApp(
+      // home: HomePage(),
+      // themeMode: ThemeMode.light,
+      // theme: ThemeData(primarySwatch: Colors.teal),
+      // darkTheme:
+      //     ThemeData(brightness: Brightness.light, primarySwatch: Colors.red),
+      initialRoute: "/",
+      routes: {
+        "/": (context) => new loginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => new loginPage()
+      },
+    );
   }
 }
