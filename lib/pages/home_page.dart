d7dad4b91c82a76e918d49ac5760d2016c27645e
Vedant Tiwari <tiwarivedant.1690@gmail.com>
2021-09-03 16:05:56 +0530

    learned list view and its some funcationalities

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,16 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_f1/Widgets/drawer.dart';
+import 'package:flutter_f1/Widgets/item_widget.dart';
+import 'package:flutter_f1/models/Catalog.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final dummylist = List.generate(10, (index) => CatalogModel.products[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text("catalog app"),
       ),
-      body: Center(
-        child: Container(
-          child: Text("welcome to the flutter"),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummylist.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: dummylist[index],
+            );
+          },
         ),
       ),
       drawer: Mydrawer(),
