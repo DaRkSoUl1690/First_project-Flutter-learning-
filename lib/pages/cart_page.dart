5902a684959fb5c3c8b05c6f2fc8a7940e71e583
Vedant Tiwari <tiwarivedant.1690@gmail.com>
2021-09-08 11:52:52 +0530

    modifying cart

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -11,6 +11,55 @@ class cartPage extends StatelessWidget {
         title: "Cart".text.color(context.accentColor).make(),
       ),
       backgroundColor: context.canvasColor,
+      body: Column(
+        children: [
+          cartlist().p32().expand(),
+          Divider(),
+          cartTotal(),
+        ],
+      ),
     );
+  }
+}
+
+class cartTotal extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 200,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          ElevatedButton(
+                  style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all(context.theme.buttonColor)),
+                  onPressed: () {},
+                  child: "BUY".text.white.make())
+              .w32(context)
+        ],
+      ),
+    );
+  }
+}
+
+class cartlist extends StatefulWidget {
+  const cartlist({Key? key}) : super(key: key);
+
+  @override
+  _cartlistState createState() => _cartlistState();
+}
+
+class _cartlistState extends State<cartlist> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: 5,
+        itemBuilder: (context, index) => ListTile(
+              leading: Icon(Icons.done),
+              trailing: IconButton(
+                  onPressed: () {}, icon: Icon(Icons.remove_circle_outline)),
+                  title:"Item 1".text.make() ,
+            ));
   }
 }
